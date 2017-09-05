20f787f7be6b7bc7b88d2911da935cb909b1d88d
Energy1190 <energyneo0@gmail.com>
2017-09-05 18:09:01 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+start_app (){
+exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
+}
+
+if [ `ls old | wc -l` -eq 0 ]; then 
+start_app
+else
+for obj in $(ls /data); do
+echo "Work with ${obj}"
+rm -rf /app/${obj}
+ln -s /data/${obj} /app/${obj}
+done
+chown -R www-data:www-data /data
+start_app
+fi
